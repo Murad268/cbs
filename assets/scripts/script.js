2afa720f7f537f41d24928c4a7920f0d2475e359
Murad <agamedov94@mail.ru>
2023-07-27 14:45:08 +0400

    works

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -50,8 +50,10 @@ $('.prev-button').click(() => {
 	infinite: true,
 	arrows: false,
 	dots: false,
+  speed: 0,
   asNavFor: '.us_chosen__slider__bottom',
   appendDots: $('us_chosen__slider__bottom'),
+  swipeToSlide: false,
 })
 
 
@@ -59,18 +61,36 @@ $('.us_chosen__slider__bottom').slick({
 	infinite: true,
 	arrows: false,
 	dots: false,
+  speed: 0,
   slidesToShow: 5,
   centerMode: true,
   variableWidth: true,
   asNavFor: '.us_chosen__slider__top',
-
-
 })
 
+$('.us_chosen__slider__top').slick('slickGoTo', 3);
 
 $('.us_chosen__slider__bottom .slick-slide').on('click', function() {
   var index = $(this).index();
-  $('.us_chosen__slider__top').slick('slickGoTo', index); // Slider 2'yi tıklanan dots'a göre konumlandırın
+  $('.us_chosen__slider__top').slick('slickGoTo', index); 
 });
 
 
+
+
+function openAccordion(triggerSelectors, contentSelectors) {
+  $(document).ready(function () {
+    $(triggerSelectors).click(function () {
+      var $this = $(this);
+      var content = $this.next(contentSelectors);
+
+      // Digər accordion elementlərinin hündürlüyünü sıfıra təyin edirik
+      $(contentSelectors).not(content).slideUp();
+
+      // Basılan accordion elementinin hündürlüyünü toggle edirik (açır və bağlayır)
+      content.slideToggle();
+    });
+  });
+}
+
+openAccordion(".service__bottom__accerdeon__main", '.service__bottom__accerdeon__content')
