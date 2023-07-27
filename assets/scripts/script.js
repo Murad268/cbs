27977ec6c33777882ff0f7025befb465a2e0d625
Murad <agamedov94@mail.ru>
2023-07-27 20:44:26 +0400

    navbar__menu

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,127 +6,134 @@ $('.banner__sliders').slick({
 $('.our__team__wrapper').slick({
 	infinite: true,
 	arrows: false,
-   slidesToShow: 4,
+	slidesToShow: 4,
 	dots: false,
-   slidesToScroll: 1,
-   responsive: [
-      {
-         breakpoint: 1220, 
-         settings: {
-           slidesToShow: 3,
-       
-         }
-         
-       },
-       {
-         breakpoint: 810, 
-         settings: {
-           slidesToShow: 2,
-    
-         }
-         
-       },
-       {
-         breakpoint: 600, 
-         settings: {
-           slidesToShow: 1,
-           arrows: false
-         }
-         
-       },
-   ]
+	slidesToScroll: 1,
+	responsive: [
+		{
+			breakpoint: 1220,
+			settings: {
+				slidesToShow: 3,
+			},
+		},
+		{
+			breakpoint: 810,
+			settings: {
+				slidesToShow: 2,
+			},
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 1,
+				arrows: false,
+			},
+		},
+	],
 })
 $('.prev-button').click(() => {
-   $('.our__team__wrapper').slick('slickPrev');
- });
- 
- $('.next-button').click(() => {
-   $('.our__team__wrapper').slick('slickNext');
- });
+	$('.our__team__wrapper').slick('slickPrev')
+})
 
+$('.next-button').click(() => {
+	$('.our__team__wrapper').slick('slickNext')
+})
 
-
- $('.us_chosen__slider__top').slick({
+$('.us_chosen__slider__top').slick({
 	infinite: true,
 	arrows: false,
 	dots: false,
-  // speed: 0,
-  asNavFor: '.us_chosen__slider__bottom',
-  appendDots: $('us_chosen__slider__bottom'),
-  swipeToSlide: false,
+	// speed: 0,
+	asNavFor: '.us_chosen__slider__bottom',
+	appendDots: $('us_chosen__slider__bottom'),
+	swipeToSlide: false,
 })
-
 
 $('.us_chosen__slider__bottom').slick({
 	infinite: true,
 	arrows: false,
 	dots: false,
-  // speed: 0,
-  slidesToShow: 5,
-  centerMode: true,
-  variableWidth: true,
-  asNavFor: '.us_chosen__slider__top',
+	// speed: 0,
+	slidesToShow: 5,
+	centerMode: true,
+	variableWidth: true,
+	asNavFor: '.us_chosen__slider__top',
 })
 
-$('.us_chosen__slider__top').slick('slickGoTo', 3);
+$('.us_chosen__slider__top').slick('slickGoTo', 3)
 
-$('.us_chosen__slider__bottom .slick-slide').on('click', function() {
-  var index = $(this).index();
-  $('.us_chosen__slider__top').slick('slickGoTo', index); 
-});
-
-
-
+$('.us_chosen__slider__bottom .slick-slide').on('click', function () {
+	var index = $(this).index()
+	$('.us_chosen__slider__top').slick('slickGoTo', index)
+})
 
 function openAccordion(triggerSelectors, contentSelectors) {
-  $(document).ready(function () {
-    $(triggerSelectors).click(function () {
-      var $this = $(this);
-      var content = $this.next(contentSelectors);
+	$(document).ready(function () {
+		$(triggerSelectors).click(function () {
+			var $this = $(this)
+			var content = $this.next(contentSelectors)
 
-      // Digər accordion elementlərinin hündürlüyünü sıfıra təyin edirik
-      $(contentSelectors).not(content).slideUp();
+			// Digər accordion elementlərinin hündürlüyünü sıfıra təyin edirik
+			$(contentSelectors).not(content).slideUp()
 
-      // Basılan accordion elementinin hündürlüyünü toggle edirik (açır və bağlayır)
-      content.slideToggle();
-    });
-  });
+			// Basılan accordion elementinin hündürlüyünü toggle edirik (açır və bağlayır)
+			content.slideToggle()
+		})
+	})
 }
 
-openAccordion(".service__bottom__accerdeon__main", '.service__bottom__accerdeon__content')
-
-
+openAccordion(
+	'.service__bottom__accerdeon__main',
+	'.service__bottom__accerdeon__content'
+)
 
 $('.recentPosts__slider').slick({
 	infinite: true,
 	arrows: false,
-   slidesToShow: 3,
+	slidesToShow: 3,
 	dots: false,
-   slidesToScroll: 1,
-   responsive: [
-      {
-         breakpoint: 1220, 
-         settings: {
-           slidesToShow: 3,
-       
-         }
-         
-       },
-       {
-         breakpoint: 810, 
-         settings: {
-           slidesToShow: 2,
-    
-         }
-         
-       },
-       {
-         breakpoint: 600, 
-         settings: {
-           slidesToShow: 1,
-           arrows: false
-         }
-         
-       },
-   ]
+	slidesToScroll: 1,
+	responsive: [
+		{
+			breakpoint: 1220,
+			settings: {
+				slidesToShow: 3,
+			},
+		},
+		{
+			breakpoint: 810,
+			settings: {
+				slidesToShow: 2,
+			},
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 1,
+				arrows: false,
+			},
+		},
+	],
 })
+
+function openHoverCursor(className, menuClass) {
+  const menu = document.querySelector(menuClass);
+  const links = document.querySelectorAll(className);
+
+  document.addEventListener('click', (e) => {
+
+    if (!e.target.closest('.header__services')) {
+      menu.classList.remove('active');
+    }
+  });
+
+  links.forEach((link) => {
+    link.addEventListener('mouseover', (e) => {
+      menu.classList.add('active');
+    });
+  });
+}
+
+openHoverCursor('.our__services span', '.header__services');
+
+
